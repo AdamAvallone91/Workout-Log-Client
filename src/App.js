11b86ee93ce219c103ae84a3a59cd028cef9c14c
Adam <Adam.avallone91@gmail.com>
2021-07-08 20:24:28 -0400

    Workout-log-client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import SiteBar from './home/Navbar';
-import React, {useState, useEffect} from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Sitebar from './home/Navbar';
 import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
 
 function App() {
-  const [sessionToken, setSessionToken] = useState(''); //1
-useEffect(() => { //2
-if (localStorage.getItem('token')){
-setSessionToken(localStorage.getItem('token'));
-}
-}, [])
-const updateToken = (newToken) = > { //3
-localStorage.setItem('token', newToken);
-setSessionToken(newToken);
-console.log(sessionToken);
-}
-//render method is down here 
-  return (
-  <div>
-  <Sitebar/>
-  </div>
-  );
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  };
+
+  const protectedViews = () => {
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken} />
+      : <Auth updateToken={updateToken} />)
   }
+
+  return (
+    <div>
+      <Sitebar clickLogout={clearToken} />
+      {protectedViews()}
+    </div>
+  );
+};
+
+export default App;
--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap'; //1
-const Auth = (props) => { //2
-return(
-<Container className="auth-container'>
-<Row>
-<Col md="6">
-Signup
-</Col>
-<Col md="6" className="login-col">
-Login
-</Col>
-</Row>
-</Container>
-)
-}
+import {Container, Row, Col} from 'reactstrap';
+import Signup from './Signup';
+import Login from './Login';
+
+const Auth = (props) => {
+    return(
+        <Container className='auth-container'>
+            <Row>
+                <Col md='6' className='signup-col'>
+                    <Signup updateToken={props.updateToken}/>
+                </Col>
+                <Col md='6' className='login-col'>
+                    <Login updateToken={props.updateToken}/>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
 export default Auth;
